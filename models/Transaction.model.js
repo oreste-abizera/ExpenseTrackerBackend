a9375c73f65512b57e0ae059ca74b79080230a87
oreste-abizera <oresteabizera11@gmail.com>
2021-02-27 11:48:25 +0200

    Transaction model

---
+++ models/Transaction.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: [true, "Add amount for this transaction"],
+  },
+  type: {
+    type: String,
+    required: [true, "Transaction type is required"],
+    enum: ["income", "expense"],
+  },
+  mode: {
+    type: String,
+    required: false,
+  },
+  date: {
+    type: Date,
+    default: new Date.now(),
+  },
+  note: {
+    type: String,
+  },
+  category: {
+    type: mongoose.Schema.ObjectId,
+    required: [true, "Category of the transaction is required."],
+    ref: "Category",
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    required: [true, "Transaction Creator is required"],
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
